7fc376ff37d5cf51b1f56af10c7cd2397ce56428
RaviiiSharma <engineerravi@036>
2026-07-05 20:28:09 +0530

    Initial commit

---
+++ Backend/src/react-archicture.js
@@ -0,0 +1,15 @@
+// 4 layers architrcture of REACT
+
+// UI=>
+// => components
+// => pages
+
+// HOOK  => for managing state and api layers
+
+// STATE => data store
+// => auth.context.jsx
+// =>ai.context.jsx
+
+// API => for communication with backend
+// => services
+// => auth.api.js
