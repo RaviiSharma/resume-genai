d982216b0782c2d62992a78a1c314e685937424b
RaviiiSharma <engineerravi@036>
2026-07-07 16:16:22 +0530

    Downgrade Puppeteer for Render compatibility

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -135,33 +135,33 @@ return validated;
 }
 
 async function generatePdfFromHtml(htmlContent) {
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+    ],
+  });
 
-  console.log("Chrome Path:", puppeteer.executablePath());
+  const page = await browser.newPage();
 
-const browser = await puppeteer.launch({
-  executablePath: puppeteer.executablePath(),
-  headless: true,
-  args: [
-    "--no-sandbox",
-    "--disable-setuid-sandbox",
-    "--disable-dev-shm-usage",
-  ],
-});
-    const page = await browser.newPage();
-    await page.setContent(htmlContent, { waitUntil: "networkidle0" })
+  await page.setContent(htmlContent, {
+    waitUntil: "networkidle0",
+  });
 
-    const pdfBuffer = await page.pdf({
-        format: "A4", margin: {
-            top: "20mm",
-            bottom: "20mm",
-            left: "15mm",
-            right: "15mm"
-        }
-    })
+  const pdfBuffer = await page.pdf({
+    format: "A4",
+    margin: {
+      top: "20mm",
+      bottom: "20mm",
+      left: "15mm",
+      right: "15mm",
+    },
+  });
 
-    await browser.close()
-
-    return pdfBuffer
+  await browser.close();
+  return pdfBuffer;
 }
 
 async function generateResumePdf({ resume, selfDescription, jobDescription }) {
